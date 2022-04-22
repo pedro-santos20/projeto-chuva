da4cba53b879e3a838e1b099715895c356b958ac
pedro santos <ps3979187@gmail.com>
2022-04-22 00:46:06 -0300

    Alterações feitas no projeto como pedido no email

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,18 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  $("#formulario").hide();
+  $("#agradecimento").hide();
 
+  $("#btnCriarTopico").click(function () {
+    $("#formulario").show();
+    $("#criarTopico").hide();
+  });
 
-    $('#formulario').hide();
+  $("#btnEnviar").click(function () {
+    $("#formulario").hide();
+    $("#agradecimento").show();
+    $("#assunto").val("");
+    $("#conteudo").val("");
+  });
 
-    $("#btnCriarTopico").click(function() {
-        $('#formulario').show();
-        $('#criarTopico').hide();
-    });
-
-    $("#btnEnviar").click(function() {
-        $('#formulario').hide();
-        $('#criarTopico').show();
-        $('#assunto').val("");
-        $('#conteudo').val("");
-
-    });
+  $("#btnNovoTopico").click(function(){
+    $("#formulario").show()
+    $("#agradecimento").hide()
+  })
 });
